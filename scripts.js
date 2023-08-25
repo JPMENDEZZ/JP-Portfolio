af34fc77d53b46ac10af8a64bdb4358cd717edec
JPMENDEZZ <joaomendezz83@gmail.com>
2023-08-25 14:50:29 -0300

    subindo portfolio

---
+++ scripts.js
@@ -0,0 +1,69 @@
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["front-end", "back-end ", "full-stack"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        if (!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    }
+    else {
+        cursorSpan.classList.remove("typing");
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+const btn = document.getElementById("btn-topo")
+
+btn.addEventListener("click", function () {
+    window.scrollTo(0, 0)
+})
+
+document.addEventListener('scroll', ocultar)
+
+function ocultar() {
+    if (window.scrollY > 10) {
+        btn.style.display = "flex"
+    } else {
+        btn.style.display = "none"
+    }
+}
+
+ocultar()
+
+const menuDiv = document.getElementById('menu-mobile-links')
+const btnAnimar = document.getElementById('btn-menu')
+
+menuDiv.addEventListener('click', animarMenu)
+
+function animarMenu(){
+    menuDiv.classList.toggle('abrir')
+    btnAnimar.classList.toggle('ativo')
+}
