510c9b019d71f53381abe1bc5c363a020f3d0e54
João Pedro Mendes <124535140+JPMENDEZZ@users.noreply.github.com>
2023-11-02 22:06:54 -0300

    Update scripts.js
    modificação na área de atuação. Excluindo Back-End e Full-Stack.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["front-end", "back-end ", "full-stack"];
+const textArray = ["front-end"];
 const typingDelay = 200;
 const erasingDelay = 100;
 const newTextDelay = 2000;
